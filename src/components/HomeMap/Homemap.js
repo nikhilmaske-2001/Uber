536aaf84c038fd630719fd69797387aa546fecbf
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-30 20:06:10 +0530

    Homemap: Structure the homemap element and render it in Homescreen.js.

---
+++ src/components/HomeMap/Homemap.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HomeMap = (props) => {
+    return (
+        <View style={{
+            backgroundColor: '#a0abff',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: 300,
+        }}>
+            <Text> This is Map! </Text>
+        </View>
+    );
+};
+
+export default HomeMap;
