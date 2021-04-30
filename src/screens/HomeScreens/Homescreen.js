53f661b9c4dc388fcabc1825e0ab4cb6f54fcd94
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-04-30 19:29:48 +0530

    Homescreen: Structured Homescreen screen and render it in App.js.

---
+++ src/screens/HomeScreens/Homescreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const HomeScreen = (props) => {
+    return (
+        <View>
+            <Text> This is HomeScreen! I am coding </Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
