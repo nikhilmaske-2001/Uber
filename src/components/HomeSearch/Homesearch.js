26290a4d410b341f02029b3eda18e8c50b6db2e9
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-01 11:17:08 +0530

    Homesearch: Style the homesearch components.

--- src/components/HomeSearch/Homesearch.js
+++ src/components/HomeSearch/Homesearch.js
@@ -5,16 +5,21 @@ import AntDesign from 'react-native-vector-icons/AntDesign';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 
+import styles from '../../styles/HomeSearch';
+
+
 const Homesearch = (props) => {
     return (
         <View>
-            <Text>Where to?</Text>
-            <View>
-                <AntDesign name={'clockcircle'} size={16} />
-                <Text>Now</Text>
-                <MaterialIcons name={'keyboard-arrow-down'} size={16} />
+            <View style={styles.inputBox}>
+                <Text style={styles.inputText}>Where to?</Text>
+                <View style={styles.timeContainer}>
+                    <AntDesign name={'clockcircle'} size={16} color='#535353' />
+                    <Text>Now</Text>
+                    <MaterialIcons name={'keyboard-arrow-down'} size={16} />
+                </View>
             </View>
-        </View>
+        </View >
     );
 };
 
--- src/styles/HomeSearch.js
+++ src/styles/HomeSearch.js
@@ -1,7 +1,28 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-
+    inputBox: {
+        backgroundColor: '#e7e7e7',
+        flexDirection: 'row',
+        justifyContent: "space-between",
+        alignContent: 'center',
+        padding: 10,
+        margin: 10,
+    },
+    inputText: {
+        fontWeight: '600',
+        fontSize: 20,
+        color: '#434343',
+        top: 5,
+    },
+    timeContainer: {
+        flexDirection: 'row',
+        backgroundColor: '#fff',
+        width: 100,
+        padding: 10,
+        borderRadius: 50,
+        justifyContent: 'space-between',
+    }
 });
 
 export default styles;
