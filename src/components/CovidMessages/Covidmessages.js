a33b8ebe3b4a391dc2b88811aa080fe542195362
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-01 10:08:10 +0530

    covidmessage: Structure covid messages files.

---
+++ src/components/CovidMessages/Covidmessages.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Covidmessages = () => {
+    return (
+        <View>
+            <Text>Travel only if necessary</Text>
+            <Text>Help flatten the curve, if you must travel, please exercise caution for your safety and our community.</Text>
+            <Text>Learn more</Text>
+        </View>
+    );
+};
+
+export default Covidmessages;
