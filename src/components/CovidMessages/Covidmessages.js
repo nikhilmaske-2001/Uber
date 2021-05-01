079e3e4ec846225dca7f457a2ba98bad20558143
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-01 10:32:41 +0530

    CovidMessages: Style the covidmessage section.

--- src/components/CovidMessages/Covidmessages.js
+++ src/components/CovidMessages/Covidmessages.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import styles from '../../styles/CovidMessages';
+
 const Covidmessages = () => {
     return (
-        <View>
-            <Text>Travel only if necessary</Text>
-            <Text>Help flatten the curve, if you must travel, please exercise caution for your safety and our community.</Text>
-            <Text>Learn more</Text>
+        <View style={styles.container}>
+            <Text style={styles.titleText}>Travel only if necessary</Text>
+            <Text style={styles.midText}>Help flatten the curve, if you must travel, please exercise caution for your safety and our community.</Text>
+            <Text style={styles.learnMore}>Learn more</Text>
         </View>
     );
 };
---
+++ src/styles/CovidMessages.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#1065e9',
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
+        padding: 10,
+    },
+    titleText: {
+        fontSize: 20,
+        color: 'white',
+        marginBottom: 10,
+        fontWeight: 'bold',
+    },
+    midText: {
+        color: 'white',
+        marginBottom: 10,
+        color: '#bed9ff',
+        fontSize: 15,
+    },
+    learnMore: {
+        color: 'white',
+        fontSize: 15,
+        fontWeight: 'bold',
+    }
+});
+
+export default styles;
