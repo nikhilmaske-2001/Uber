56b0387bda6d9959f55fb5f82fc6211d1db53217
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-01 10:08:10 +0530

    covidmessage: Structure covid messages files.

--- src/screens/HomeScreens/Homescreen.js
+++ src/screens/HomeScreens/Homescreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Covidmessages from '../../components/CovidMessages/Covidmessages';
 
 import HomeMap from '../../components/HomeMap/Homemap';
+import CovidMessages from '../../components/CovidMessages/Covidmessages';
 
 const HomeScreen = (props) => {
     return (
         <View>
             <HomeMap />
+            <CovidMessages />
         </View>
     );
 };
