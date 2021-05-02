d31b083a031366962f2c799144f98d22412f77c0
nikhilmaske-2001 <59444243+nikhilmaske-2001@users.noreply.github.com>
2021-05-02 11:05:44 +0530

    useState: Add a states to store the history values.

--- src/screens/DestinationSearch/DestinationSearch.js
+++ src/screens/DestinationSearch/DestinationSearch.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, View, SafeAreaView, TextInput } from 'react-native';
 
 import styles from '../../styles/DestinationSearch';
 
 
+
 const DestinationSearch = (props) => {
+    // TO store the text from "From" placeholder
+    const [fromText, setFromText] = useState({ initialState: '' });
+
+    // TO store the text from "Where to ?" placeholder
+    const [DestinationText, setDestinationText] = useState({ initialState: '' });
+
     return (
         <SafeAreaView>
             <View style={styles.container}>
-                <TextInput placeholder="From" style={styles.textInput}></TextInput>
-                <TextInput placeholder="Where to?" style={styles.textInput}></TextInput>
+                <TextInput
+                    value={fromText}
+                    onChangeText={setFromText}
+                    placeholder="From"
+                    style={styles.textInput}>
+                </TextInput>
+                <TextInput
+                    value={DestinationText}
+                    onChangeText={setDestinationText}
+                    placeholder="Where to?"
+                    style={styles.textInput}></TextInput>
             </View>
         </SafeAreaView>
     );
